0adc9e3edd4aa426e52a77c0661ccf579124723b
小陳 <tim.minecraft2017@gmail.com>
2024-06-15 14:43:32 +0800

    Initial Commit

---
+++ first_peoject/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  first_peoject
+//
+//  Created by Contestant on 2024/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var n : Int = 10
+    var n1 : Int = 20
+    var body: some View {
+        Text("123")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first_peoject/first_peojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_peojectApp.swift
+//  first_peoject
+//
+//  Created by Contestant on 2024/4/27.
+//
+
+import SwiftUI
+
+@main
+struct first_peojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
